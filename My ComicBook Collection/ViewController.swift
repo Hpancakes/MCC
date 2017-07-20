a6d13fbbf0489781f62e12469bf4f4d699619bba
Taslot <jluis.larav@gmail.com>
2017-07-19 23:47:33 -0500

    Funcionado con details

--- My ComicBook Collection/ViewController.swift
+++ My ComicBook Collection/ViewController.swift
@@ -8,17 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var comics : [ComicBook] = []
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+        comics = try context.fetch(ComicBook.fetchRequest())
+            tableView.reloadData()
+        } catch{
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comics.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let comic = comics[indexPath.row]
+        let comicNumero = comics[indexPath.row]
+        cell.textLabel?.text = comic.comicbName
+        cell.detailTextLabel?.text = comicNumero.numbers
+        return cell
+    }
+    
 
 
 }
